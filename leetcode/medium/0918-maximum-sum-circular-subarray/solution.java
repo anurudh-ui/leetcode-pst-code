83991fbe111b5aa4ed34bf13c236b4cc4776e2b4
anurudh-ui <anurudhparupalli@gmail.com>
2026-09-16 09:36:58 +0530

    ✅ Maximum Sum Circular Subarray | Medium | Java | Runtime: 7 ms (50.82%) | Memory: 50.7 MB | 2026-09-16

---
+++ leetcode/medium/0918-maximum-sum-circular-subarray/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxSubarraySumCircular(int[] nums) {
+        int totalSum = 0;
+        int currMax = 0, maxSum = nums[0];
+        int currMin = 0, minSum = nums[0];
+        
+        for (int num : nums) {
+
+            currMax = Math.max(currMax + num, num);
+            maxSum = Math.max(maxSum, currMax);
+            
+
+            currMin = Math.min(currMin + num, num);
+            minSum = Math.min(minSum, currMin);
+            
+            totalSum += num;
+        }
+        if (maxSum < 0) {
+            return maxSum;
+        }
+        
+        return Math.max(maxSum, totalSum - minSum);
+    }
+}
